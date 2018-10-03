688d0966c275ee6f33329fdcc0a44529493b0e4a
Robby <robby@k-state.edu>
2018-10-03 10:41:52 -0500

    Added Tipe as part of CI tests.

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
 export SCRIPT_HOME=$( cd "$( dirname "$0" )" &> /dev/null && pwd )
-rm -fR runtime mill-standalone versions.properties out
+rm -fR runtime mill-standalone versions.properties out sireum
+curl -Lo sireum http://files.sireum.org/sireum
+chmod +x sireum
+$SCRIPT_HOME/sireum slang tipe --verbose -r -s library
 curl -Lo mill-standalone http://files.sireum.org/mill-standalone
 chmod +x mill-standalone
 curl -Lo versions.properties https://raw.githubusercontent.com/sireum/kekinian/master/versions.properties
