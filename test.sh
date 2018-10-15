11ef52008897fc00ae28ea4b8623c58bd47f3174
Robby <robby@k-state.edu>
2018-10-14 21:21:01 -0500

    Refactored test.sh.

--- test.sh
+++ test.sh
@@ -1,12 +1,7 @@
 #!/bin/bash -e
 export SCRIPT_HOME=$( cd "$( dirname "$0" )" &> /dev/null && pwd )
-rm -fR runtime mill-standalone versions.properties out sireum
-curl -Lo sireum http://files.sireum.org/sireum
-chmod +x sireum
+$SCRIPT_HOME/prelude.sh
 $SCRIPT_HOME/sireum slang tipe --verbose -r -s library
-curl -Lo mill-standalone http://files.sireum.org/mill-standalone
-chmod +x mill-standalone
-curl -Lo versions.properties https://raw.githubusercontent.com/sireum/kekinian/master/versions.properties
 $SCRIPT_HOME/mill-standalone all runtime.library.shared.tests.test runtime.library.jvm.tests.test
 if [ -x "$(command -v node)" ]; then
   $SCRIPT_HOME/mill-standalone runtime.library.js.tests.test
