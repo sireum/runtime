eb3a940a19d0d8f99d330df9c85435928005a32f
Robby <robby@k-state.edu>
2019-09-13 10:22:57 -0600

    Added contracts on CircularQueue.

--- library/shared/src/main/scala/org/sireum/CircularQueue.scala
+++ library/shared/src/main/scala/org/sireum/CircularQueue.scala
@@ -25,21 +25,68 @@
  */
 package org.sireum
 
+import ContractUtil._
 
 @record trait CircularQueue[E] {
-  def size: Z
 
-  def isEmpty: B
+  @spec var rep: MSZ[E] = $
 
-  def nonEmpty: B
+  @pure def max: Z
 
-  def isFull: B
+  @pure def default: E
 
-  def enqueue(element: E): Unit
+  @pure def scrub: B
 
-  def dequeue(): E
+  @pure def policy: CircularQueue.Policy.Type
 
-  def elements: MSZ[E]
+  def size: Z = Contract.Only(Ensures(Res == rep.size))
+
+  def isEmpty: B = Contract.Only(Ensures(Res == (rep.size == 0)))
+
+  def nonEmpty: B = Contract.Only(Ensures(Res == (rep.size != 0)))
+
+  def isFull: B = Contract.Only(Ensures(Res == (rep.size == max)))
+
+  def enqueue(element: E): Unit = Contract.Only(
+    Modifies(rep),
+    Case("Non-full",
+      Requires(rep.size < max),
+      Ensures(rep == In(rep) :+ element)
+    ),
+    Case("Drop front policy and full",
+      Requires(
+        policy == CircularQueue.Policy.DropFront,
+        rep.size == max
+      ),
+      Ensures(
+        rep.size == In(rep).size,
+        All(1 until rep.size)(i => rep(i - 1) == In(rep)(i)),
+        rep(rep.size - 1) == element
+      )
+    ),
+    Case("Drop rear policy and full",
+      Requires(
+        policy == CircularQueue.Policy.DropRear,
+        rep.size == max
+      ),
+      Ensures(
+        rep.size == In(rep).size,
+        msEqualExcept(rep, In(rep), rep.size - 1),
+        rep(rep.size - 1) == element
+      )
+    )
+  )
+
+  def dequeue(): E = Contract.Only(
+    Requires(rep.size > 0),
+    Modifies(rep),
+    Ensures(
+      rep.size == In(rep).size - 1,
+      All(1 until In(rep).size)(i => rep(i - 1) == In(rep)(i))
+    )
+  )
+
+  def elements: MSZ[E] = Contract.Only(Ensures(Res == rep))
 
   override def string: String = {
     return elements.string
@@ -54,175 +101,422 @@ object CircularQueue {
     'DropRear
   }
 
-  @record class NoDrop[E](max: Z,
-                          default: E,
-                          clean: B,
+  @strictpure def inv[E](max: Z, default: E, scrub: B, queue: MSZ[E], front: Z, rear: Z, numOfElements: Z): B =
+    max > 0 &
+      max + 1 == queue.size &
+      queue.isInBound(front) &
+      queue.isInBound(rear) &
+      0 <= numOfElements &
+      numOfElements <= max &
+      (rear >= front) === (numOfElements == rear - front) &
+      (rear < front) === (numOfElements == rear + queue.size - front) &
+      (scrub imply_: All(0 until queue.size - numOfElements)(i => queue(modPos(rear + i, queue.size)) == default))
+
+  @strictpure def refinement[E](rep: MSZ[E], queue: MSZ[E], numOfElements: Z, front: Z): B =
+    rep.size == numOfElements &
+      All(rep.indices)(i => rep(i) == queue(modPos(front + i, queue.size)))
+
+  @strictpure def createEnsures[E](res: CircularQueue[E], max: Z, default: E, scrub: B, policy: Policy.Type): B =
+    res.max == max &
+      res.default == default &
+      res.scrub == scrub &
+      res.policy == policy &
+      res.size == 0
+
+  object NoDrop {
+
+    @pure def create[E](max: Z, default: E, scrub: B): NoDrop[E] = {
+      Contract(
+        Ensures(
+          createEnsures(Res, max, default, scrub, Policy.NoDrop) &
+            additionalCreateEnsures(Res) &
+            Res[NoDrop[E]].rep == MSZ[E]()
+        )
+      )
+
+      return NoDrop(max, default, scrub, MS.create(max + 1, default), 0, 0, 0)
+    }
+
+    @strictpure def additionalCreateEnsures[E](res: NoDrop[E]): B =
+      res.front == 0 &
+        res.rear == 0 &
+        res.numOfElements == 0 &
+        isAllMS(res.queue, res.default)
+  }
+
+  @record class NoDrop[E](val max: Z,
+                          val default: E,
+                          val scrub: B,
                           queue: MSZ[E],
                           var front: Z,
                           var rear: Z,
                           var numOfElements: Z) extends CircularQueue[E] {
 
-    def size: Z = {
+    @spec def invariant = Invariant(
+      inv(max, default, scrub, queue, front, rear, numOfElements)
+    )
+
+    Contract(
+      DataRefinement(rep)(queue, front, rear, numOfElements)(
+        refinement(rep, queue, numOfElements, front)
+      )
+    )
+
+    @strictpure def policy: Policy.Type = Policy.NoDrop
+
+    override def size: Z = {
+      Contract(Ensures(Res == numOfElements))
+
       return numOfElements
     }
 
-    def isEmpty: B = {
+    override def isEmpty: B = {
+      Contract(Ensures(Res == (numOfElements == 0)))
+
       return numOfElements == 0
     }
 
-    def nonEmpty: B = {
+    override def nonEmpty: B = {
+      Contract(Ensures(Res == (numOfElements != 0)))
+
       return !isEmpty
     }
 
-    def isFull: B = {
+    override def isFull: B = {
+      Contract(Ensures(Res == (numOfElements == max)))
+
       return numOfElements == max
     }
 
-    def enqueue(element: E): Unit = {
-      assert(!isFull)
+    override def enqueue(element: E): Unit = {
+      Contract(
+        Requires(numOfElements != max),
+        Modifies(queue, rear, front, numOfElements),
+        Ensures(
+          queue(In(rear)) == element,
+          rear == modPos(In(rear) + 1, queue.size),
+          numOfElements == In(numOfElements) + 1,
+          msEqualExcept(queue, In(queue), In(rear))
+        )
+      )
+
       queue(rear) = element
-      rear = (rear + 1) % queue.size
+      rear = modPos(rear + 1, queue.size)
       numOfElements = numOfElements + 1
     }
 
-    def dequeue(): E = {
-      assert(!isEmpty)
+    override def dequeue(): E = {
+      Contract(
+        Requires(numOfElements != 0),
+        Modifies(queue, front, numOfElements),
+        Ensures(
+          numOfElements == In(numOfElements) - 1,
+          front == modPos(In(front) + 1, queue.size),
+          Res == queue(In(front))
+        )
+      )
+
       val r = queue(front)
-      if (clean) {
+      if (scrub) {
         queue(front) = default
       }
-      front = (front + 1) % queue.size
+      front = modPos(front + 1, queue.size)
       numOfElements = numOfElements - 1
       return r
     }
 
-    def elements: MSZ[E] = {
+    override def elements: MSZ[E] = {
+      Contract(Ensures(refinement(Res, queue, numOfElements, front)))
+
       val r = MS.create(numOfElements, default)
       for (i <- 0 until numOfElements) {
-        r(i) = queue((front + i) % queue.size)
+        r(i) = queue(modPos(front + i, queue.size))
       }
       return r
     }
   }
 
-  @record class DropFront[E](max: Z,
-                             default: E,
-                             clean: B,
+  object DropFront {
+
+    @pure def create[E](max: Z, default: E, scrub: B): DropFront[E] = {
+      Contract(
+        Ensures(
+          createEnsures(Res, max, default, scrub, Policy.NoDrop) &
+            additionalCreateEnsures(Res) &
+            Res[DropFront[E]].rep == MSZ[E]()
+        )
+      )
+
+      return DropFront(max, default, scrub, MS.create(max + 1, default), 0, 0, 0)
+    }
+
+    @strictpure def additionalCreateEnsures[E](res: DropFront[E]): B =
+      res.front == 0 &
+        res.rear == 0 &
+        res.numOfElements == 0 &
+        isAllMS(res.queue, res.default)
+  }
+
+  @record class DropFront[E](val max: Z,
+                             val default: E,
+                             val scrub: B,
                              queue: MSZ[E],
                              var front: Z,
                              var rear: Z,
                              var numOfElements: Z) extends CircularQueue[E] {
 
-    def size: Z = {
+    @spec def invariant = Invariant(
+      inv(max, default, scrub, queue, front, rear, numOfElements)
+    )
+
+    Contract(
+      DataRefinement(rep)(queue, front, rear, numOfElements)(
+        refinement(rep, queue, numOfElements, front)
+      )
+    )
+
+    @strictpure def policy: Policy.Type = Policy.DropFront
+
+    override def size: Z = {
+      Contract(Ensures(Res == numOfElements))
+
       return numOfElements
     }
 
-    def isEmpty: B = {
+    override def isEmpty: B = {
+      Contract(Ensures(Res == (numOfElements == 0)))
+
       return numOfElements == 0
     }
 
-    def nonEmpty: B = {
+    override def nonEmpty: B = {
+      Contract(Ensures(Res == (numOfElements != 0)))
+
       return !isEmpty
     }
 
-    def isFull: B = {
+    override def isFull: B = {
+      Contract(Ensures(Res == (numOfElements == max)))
+
       return numOfElements == max
     }
 
-    def enqueue(element: E): Unit = {
+    override def enqueue(element: E): Unit = {
+      Contract(
+        Modifies(queue, rear, front, numOfElements),
+        Ensures(
+          queue(In(rear)) == element,
+          rear == modPos(In(rear) + 1, queue.size),
+          (In(numOfElements) < max) === (numOfElements == In(numOfElements) + 1),
+          (In(numOfElements) == max) === (numOfElements == In(numOfElements)),
+          msEqualExcept(queue, In(queue), In(rear))
+        )
+      )
+
       if (isFull) {
         dequeue()
       }
       queue(rear) = element
-      rear = (rear + 1) % queue.size
+      rear = modPos(rear + 1, queue.size)
       numOfElements = numOfElements + 1
     }
 
-    def dequeue(): E = {
-      assert(!isEmpty)
+    override def dequeue(): E = {
+      Contract(
+        Requires(numOfElements != 0),
+        Modifies(queue, front, numOfElements),
+        Ensures(
+          numOfElements == In(numOfElements) - 1,
+          front == modPos(In(front) + 1, queue.size),
+          Res == queue(In(front))
+        )
+      )
+
       val r = queue(front)
-      if (clean) {
+      if (scrub) {
         queue(front) = default
       }
-      front = (front + 1) % queue.size
+      front = modPos(front + 1, queue.size)
       numOfElements = numOfElements - 1
       return r
     }
 
-    def elements: MSZ[E] = {
+    override def elements: MSZ[E] = {
+      Contract(Ensures(refinement(Res, queue, numOfElements, front)))
+
       val r = MS.create(numOfElements, default)
       for (i <- 0 until numOfElements) {
-        r(i) = queue((front + i) % queue.size)
+        r(i) = queue(modPos(front + i, queue.size))
       }
       return r
     }
   }
 
-  @record class DropRear[E](max: Z,
-                            default: E,
-                            clean: B,
+  object DropRear {
+
+    @pure def create[E](max: Z, default: E, scrub: B): DropRear[E] = {
+      Contract(
+        Ensures(
+          createEnsures(Res, max, default, scrub, Policy.NoDrop) &
+            additionalCreateEnsures(Res) &
+            Res[DropRear[E]].rep == MSZ[E]()
+        )
+      )
+      return DropRear(max, default, scrub, MS.create(max + 1, default), 0, 0, 0)
+    }
+
+    @strictpure def additionalCreateEnsures[E](res: DropRear[E]): B =
+      res.front == 0 &
+        res.rear == 0 &
+        res.numOfElements == 0 &
+        isAllMS(res.queue, res.default)
+  }
+
+  @record class DropRear[E](val max: Z,
+                            val default: E,
+                            val scrub: B,
                             queue: MSZ[E],
                             var front: Z,
                             var rear: Z,
                             var numOfElements: Z) extends CircularQueue[E] {
 
-    def size: Z = {
+    @spec def invariant = Invariant(
+      inv(max, default, scrub, queue, front, rear, numOfElements)
+    )
+
+    Contract(
+      DataRefinement(rep)(queue, front, rear, numOfElements)(
+        refinement(rep, queue, numOfElements, front)
+      )
+    )
+
+    @strictpure def policy: Policy.Type = Policy.DropRear
+
+    override def size: Z = {
+      Contract(Ensures(Res == numOfElements))
+
       return numOfElements
     }
 
-    def isEmpty: B = {
+    override def isEmpty: B = {
+      Contract(Ensures(Res == (numOfElements == 0)))
+
       return numOfElements == 0
     }
 
-    def nonEmpty: B = {
+    override def nonEmpty: B = {
+      Contract(Ensures(Res == (numOfElements != 0)))
+
       return !isEmpty
     }
 
-    def isFull: B = {
+    override def isFull: B = {
+      Contract(Ensures(Res == (numOfElements == max)))
+
       return numOfElements == max
     }
 
-    def enqueue(element: E): Unit = {
+    override def enqueue(element: E): Unit = {
+      Contract(
+        Modifies(queue, rear, front, numOfElements),
+        Case("Non-full and rear is not the last index",
+          Requires(
+            In(numOfElements) < max,
+            rear < max
+          ),
+          Ensures(
+            numOfElements == In(numOfElements) + 1,
+            rear == In(rear) + 1,
+            queue(In(rear)) == element,
+            msEqualExcept(queue, In(queue), In(rear))
+          )
+        ),
+        Case("Non-full and rear is the last index",
+          Requires(
+            In(numOfElements) < max,
+            rear == max
+          ),
+          Ensures(
+            numOfElements == In(numOfElements) + 1,
+            rear == 0,
+            queue(max) == element,
+            msEqualExcept(queue, In(queue), max)
+          )
+        ),
+        Case("Full and rear is the first index",
+          Requires(
+            numOfElements == max,
+            rear == 0
+          ),
+          Ensures(
+            numOfElements == In(numOfElements),
+            rear == In(rear),
+            queue(max) == element,
+            msEqualExcept(queue, In(queue), max)
+          )
+        ),
+        Case("Full and rear is not the first index",
+          Requires(
+            In(numOfElements) == max,
+            rear > 0
+          ),
+          Ensures(
+            numOfElements == In(numOfElements),
+            rear == In(rear),
+            queue(rear - 1) == element,
+            msEqualExcept(queue, In(queue), rear - 1)
+          )
+        )
+      )
+
       if (isFull) {
-        var idx = rear - 1
-        if (idx < 0) {
-          idx = idx + queue.size
-        }
-        queue(idx % queue.size) = element
+        queue(modNeg(rear - 1, queue.size)) = element
       } else {
         queue(rear) = element
-        rear = (rear + 1) % queue.size
+        rear = modPos(rear + 1, queue.size)
         numOfElements = numOfElements + 1
       }
     }
 
-    def dequeue(): E = {
-      assert(!isEmpty)
+    override def dequeue(): E = {
+      Contract(
+        Requires(numOfElements != 0),
+        Modifies(queue, front, numOfElements),
+        Ensures(
+          numOfElements == In(numOfElements) - 1,
+          front == modPos(In(front) + 1, queue.size),
+          Res == queue(In(front))
+        )
+      )
+
       val r = queue(front)
-      if (clean) {
+      if (scrub) {
         queue(front) = default
       }
-      front = (front + 1) % queue.size
+      front = modPos(front + 1, queue.size)
       numOfElements = numOfElements - 1
       return r
     }
 
-    def elements: MSZ[E] = {
+    override def elements: MSZ[E] = {
+      Contract(Ensures(refinement(Res, queue, numOfElements, front)))
+
       val r = MS.create(numOfElements, default)
       for (i <- 0 until numOfElements) {
-        r(i) = queue((front + i) % queue.size)
+        r(i) = queue(modPos(front + i, queue.size))
       }
       return r
     }
   }
 
-  def create[E](max: Z, default: E, clean: B, policy: Policy.Type): CircularQueue[E] = {
+  @pure def create[E](max: Z, default: E, scrub: B, policy: Policy.Type): CircularQueue[E] = {
+    Contract(Ensures(createEnsures(Res, max, default, scrub, policy)))
+
     policy match {
-      case Policy.NoDrop => return NoDrop(max, default, clean, MS.create(max + 1, default), 0, 0, 0)
-      case Policy.DropFront => return DropFront(max, default, clean, MS.create(max + 1, default), 0, 0, 0)
-      case Policy.DropRear => return DropRear(max, default, clean, MS.create(max + 1, default), 0, 0, 0)
+      case Policy.NoDrop => return NoDrop.create(max, default, scrub)
+      case Policy.DropFront => return DropFront.create(max, default, scrub)
+      case Policy.DropRear => return DropRear.create(max, default, scrub)
     }
   }
 
 }
-
