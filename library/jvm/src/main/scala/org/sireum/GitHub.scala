7fb8613ab2433efcdc0876c369b2a3c3b029b721
Robby <robby@k-state.edu>
2019-11-26 22:34:13 -0600

    Added GitHub API.

---
+++ library/jvm/src/main/scala/org/sireum/GitHub.scala
@@ -0,0 +1,82 @@
+// #Sireum
+/*
+ Copyright (c) 2019, Robby, Kansas State University
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.sireum
+
+object GitHub {
+
+  @datatype trait Credential
+
+  @datatype class Repository(connection: Option[Credential],
+                             owner: String,
+                             name: String) {
+    def latestRelease: Release = {
+      return Ext.latestRelease(this)
+    }
+
+    def releases: Jen[Release] = {
+      return Ext.releases(this)
+    }
+  }
+
+  @datatype class Release(repo: Repository,
+                          id: Z,
+                          name: String,
+                          publishedTime: Z,
+                          isDraft: B,
+                          isPrerelease: B,
+                          tagName: String,
+                          commit: String,
+                          tarUrl: String,
+                          zipUrl: String) {
+    def assets: Jen[Asset] = {
+      return Ext.assets(this)
+    }
+  }
+
+  @datatype class Asset(release: Release,
+                        id: Z,
+                        name: String,
+                        label: String,
+                        state: String,
+                        size: Z,
+                        contentType: String,
+                        url: String,
+                        downloadCount: Z) {
+
+  }
+
+  @strictpure def repo(owner: String, repo: String): Repository = Repository(None(), owner, repo)
+
+  @ext("GitHub_Ext") object Ext {
+
+    def latestRelease(repo: Repository): Release = $
+
+    def releases(repo: Repository): Jen[Release] = $
+
+    def assets(release: Release): Jen[Asset] = $
+  }
+}
+
---
+++ library/jvm/src/main/scala/org/sireum/GitHub_Ext.scala
@@ -0,0 +1,95 @@
+/*
+ Copyright (c) 2019, Robby, Kansas State University
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.sireum
+
+import GitHub._
+import org.kohsuke.github.{GHRelease, GHRepository, GitHub => GH}
+
+object GitHub_Ext {
+  def connectRepo(owner: String, name: String): GHRepository =
+    GH.connectAnonymously().getRepository(s"$owner/$name")
+
+  def toRelease(repo: Repository, ghRelease: GHRelease): Release =
+    Release(
+      repo,
+      ghRelease.getId,
+      ghRelease.getName,
+      ghRelease.getPublished_at.getTime,
+      ghRelease.isDraft,
+      ghRelease.isPrerelease,
+      ghRelease.getTagName,
+      ghRelease.getTargetCommitish,
+      ghRelease.getTarballUrl,
+      ghRelease.getZipballUrl
+    )
+
+  def latestRelease(repo: Repository): Release = {
+    val ghRepo = connectRepo(repo.owner, repo.name)
+    toRelease(repo, ghRepo.getLatestRelease)
+  }
+
+  def releases(repo: Repository): Jen[Release] = {
+    import scala.collection.JavaConverters._
+    val ghRepo = connectRepo(repo.owner, repo.name)
+    val ghReleases = ghRepo.listReleases
+    new Jen[Release] {
+      override def generate(f: Release => Jen.Action): Jen.Action = {
+        var last = Jen.Continue
+        for (e <- ghReleases.asScala) {
+          last = f(toRelease(repo, e))
+          if (!last) {
+            return Jen.End
+          }
+        }
+        return last
+      }
+
+      override def string: String = {
+        return s"Jen($repo.releases)"
+      }
+    }
+  }
+
+  def assets(release: Release): Jen[Asset] = {
+    import scala.collection.JavaConverters._
+    val ghRepo = connectRepo(release.repo.owner, release.repo.name)
+    val ghRelease = ghRepo.getRelease(release.id.toLong)
+    var assets = ISZ[GitHub.Asset]()
+    for (ghAsset <- ghRelease.getAssets.asScala) {
+      assets = assets :+ GitHub.Asset(
+        release,
+        ghAsset.getId,
+        ghAsset.getName,
+        ghAsset.getLabel,
+        ghAsset.getState,
+        ghAsset.getSize,
+        ghAsset.getContentType,
+        ghAsset.getBrowserDownloadUrl,
+        ghAsset.getDownloadCount
+      )
+    }
+    Jen.IS(assets)
+  }
+}
