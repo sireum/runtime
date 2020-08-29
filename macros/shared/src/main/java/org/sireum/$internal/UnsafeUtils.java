ea8c4658c7b577586e5225a7d8cee05a9a691728
Robby <robby@santoslab.org>
2020-08-29 13:46:17 -0500

    Added UnsafeUtils for calling sun.misc.Unsafe.storeFence().

---
+++ macros/shared/src/main/java/org/sireum/$internal/UnsafeUtils.java
@@ -0,0 +1,18 @@
+// From: https://github.com/plokhotnyuk/jsoniter-scala/commit/e089f06c2d8b4bdb87a6874e17bf716e8608b117#diff-3602e7991d01bab723a637f0f1b1d029
+package org.sireum.$internal;
+
+public class UnsafeUtils {
+  static final sun.misc.Unsafe UNSAFE;
+  static {
+    try {
+      java.lang.reflect.Field field = sun.misc.Unsafe.class.getDeclaredField("theUnsafe");
+      field.setAccessible(true);
+      UNSAFE = (sun.misc.Unsafe) field.get(null);
+    } catch (Throwable ex) {
+      throw new ExceptionInInitializerError(ex);
+    }
+  }
+  public static void releaseFence() {
+    UNSAFE.storeFence();
+  }
+}
