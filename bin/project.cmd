ca2ea4e5c59694231f079efae891a04d9fdf00bd
Robby <robby@santoslab.org>
2021-03-24 16:30:33 -0500

    Updated project.cmd.

--- bin/project.cmd
+++ bin/project.cmd
@@ -1,20 +1,25 @@
-::#! 2> /dev/null                                                                                           #
-@ 2>/dev/null # 2>nul & echo off & goto BOF                                                                 #
-export SIREUM_HOME=$(cd -P $(dirname "$0")/../.. && pwd -P)                                                 #
-if [ -f "$0.com" ] && [ "$0.com" -nt "$0" ]; then                                                           #
-  exec "$0.com" "$@"                                                                                        #
-else                                                                                                        #
-  rm -fR "$0.com"                                                                                           #
-  exec "${SIREUM_HOME}/bin/sireum" slang run -n "$0" "$@"                                                   #
-fi                                                                                                          #
+::#! 2> /dev/null                                             #
+@ 2>/dev/null # 2>nul & echo off & goto BOF                   #
+if [ -f "$0.com" ] && [ "$0.com" -nt "$0" ]; then             #
+  exec "$0.com" "$@"                                          #
+fi                                                            #
+rm -f "$0.com"                                                #
+if [ -z ${SIREUM_HOME} ]; then                                #
+  echo "Please set SIREUM_HOME env var"                       #
+  exit -1                                                     #
+fi                                                            #
+exec ${SIREUM_HOME}/bin/sireum slang run -n "$0" "$@"         #
 :BOF
 setlocal
+if not defined SIREUM_HOME (
+  echo Please set SIREUM_HOME env var
+  exit /B -1
+)
 set NEWER=False
 if exist %~dpnx0.com for /f %%i in ('powershell -noprofile -executionpolicy bypass -command "(Get-Item %~dpnx0.com).LastWriteTime -gt (Get-Item %~dpnx0).LastWriteTime"') do @set NEWER=%%i
 if "%NEWER%" == "True" goto native
 del "%~dpnx0.com" > nul 2>&1
-if not exist "%~dp0..\sireum.jar" call "%~dp0..\init.bat"
-"%~dp0..\sireum.bat" slang run -n "%0" %*
+%SIREUM_HOME%\bin\sireum.bat slang run -n "%0" %*
 exit /B %errorlevel%
 :native
 %~dpnx0.com %*
@@ -26,11 +31,15 @@ import org.sireum._
 import org.sireum.project.{Project, JSON}
 import org.sireum.project.ProjectUtil._
 
+val macros = "macros"
+val library = "library"
+val test = "test"
+
 val homeDir = Os.slashDir.up.canon
 
 val macrosShared = moduleShared(
-  baseId = "macros",
-  baseDir = homeDir / "macros",
+  id = macros,
+  baseDir = homeDir / macros,
   sharedDeps = ISZ(),
   sharedIvyDeps = ISZ(
     "org.scala-lang:scala-reflect:",
@@ -40,8 +49,8 @@ val macrosShared = moduleShared(
 )
 
 val (libraryShared, libraryJvm) = moduleSharedJvm(
-  baseId = "library",
-  baseDir = homeDir / "library",
+  baseId = library,
+  baseDir = homeDir / library,
   sharedDeps = ISZ(macrosShared.id),
   sharedIvyDeps = ISZ(),
   jvmDeps = ISZ(),
@@ -53,8 +62,8 @@ val (libraryShared, libraryJvm) = moduleSharedJvm(
 )
 
 val testShared = moduleShared(
-  baseId = "test",
-  baseDir = homeDir / "test",
+  id = test,
+  baseDir = homeDir / test,
   sharedDeps = ISZ(macrosShared.id),
   sharedIvyDeps = ISZ("org.scalatest::scalatest::")
 )
