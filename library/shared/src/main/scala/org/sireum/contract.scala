b4feff734fee9e918be02fee514ed3e0ec69a0f6
Robby <robby@santoslab.org>
2021-04-30 18:39:43 -0500

    Added inception eta and loop max iterations.

--- library/shared/src/main/scala/org/sireum/contract.scala
+++ library/shared/src/main/scala/org/sireum/contract.scala
@@ -143,6 +143,8 @@ trait contract {
 
     trait Ensures
 
+    trait MaxIt
+
     trait Sequent {
       def Proof(steps: ProofStep*): Sequent
     }
@@ -183,6 +185,28 @@ trait contract {
       def by(just: Justification): ProofStep
       def by(just: Predef.String): ProofStep
       def by(o: Unit): StepBuilder3
+      def by[T1](f: T1 => Unit): StepBuilder3
+      def by[T1, T2](f: (T1, T2) => Unit): StepBuilder3
+      def by[T1, T2, T3](f: (T1, T2, T3) => Unit): StepBuilder3
+      def by[T1, T2, T3, T4](f: (T1, T2, T3, T4) => Unit): StepBuilder3
+      def by[T1, T2, T3, T4, T5](f: (T1, T2, T3, T4, T5) => Unit): StepBuilder3
+      def by[T1, T2, T3, T4, T5, T6](f: (T1, T2, T3, T4, T5, T6) => Unit): StepBuilder3
+      def by[T1, T2, T3, T4, T5, T6, T7](f: (T1, T2, T3, T4, T5, T6, T7) => Unit): StepBuilder3
+      def by[T1, T2, T3, T4, T5, T6, T7, T8](f: (T1, T2, T3, T4, T5, T6, T7, T8) => Unit): StepBuilder3
+      def by[T1, T2, T3, T4, T5, T6, T7, T8, T9](f: (T1, T2, T3, T4, T5, T6, T7, T8, T9) => Unit): StepBuilder3
+      def by[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10](f: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10) => Unit): StepBuilder3
+      def by[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11](f: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11) => Unit): StepBuilder3
+      def by[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12](f: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12) => Unit): StepBuilder3
+      def by[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13](f: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13) => Unit): StepBuilder3
+      def by[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14](f: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14) => Unit): StepBuilder3
+      def by[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15](f: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15) => Unit): StepBuilder3
+      def by[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16](f: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16) => Unit): StepBuilder3
+      def by[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17](f: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17) => Unit): StepBuilder3
+      def by[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18](f: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18) => Unit): StepBuilder3
+      def by[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19](f: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19) => Unit): StepBuilder3
+      def by[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20](f: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20) => Unit): StepBuilder3
+      def by[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21](f: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21) => Unit): StepBuilder3
+      def by[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22](f: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22) => Unit): StepBuilder3
     }
 
     trait StepBuilder3 {
@@ -199,6 +223,8 @@ trait contract {
       def ⊢(conclusion: B): Sequent
     }
   }
+
+  def MaxIt(num: Z): Contract.MaxIt = ???
 
   def Reads(claims: Any*): Contract.Reads = ???
 
@@ -227,6 +253,10 @@ trait contract {
   def Case(name: String, requires: Contract.Requires, ensures: Contract.Ensures): Contract.Case = ???
 
   def Invariant(arg0: Contract.Modifies, arg1: B*): Unit = macro Macro.lUnit1S
+
+  def Invariant(arg0: Contract.MaxIt, arg1: B*): Unit = macro Macro.lUnit1S
+
+  def Invariant(arg0: Contract.MaxIt, arg1: Contract.Modifies, arg2: B*): Unit = macro Macro.lUnit2S
 
   def Fact(claims: B*): Contract.Fact = ???
 
