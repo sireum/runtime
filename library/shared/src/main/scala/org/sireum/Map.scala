112808a4603e38404f2e6e394f437a6e1e10c50e
Robby <robby@santoslab.org>
2022-04-20 15:48:07 -0500

    Fixed replace issue.

--- library/shared/src/main/scala/org/sireum/Map.scala
+++ library/shared/src/main/scala/org/sireum/Map.scala
@@ -1,7 +1,7 @@
 // #Sireum #Logika
 /*
  Copyright (c) 2017-2022, Robby, Kansas State University
- ∀ rights reserved.
+ All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
