a10b13706bd0ef2c3f7a76b0c7c2350e93ffe2d7
Robby <robby@santoslab.org>
2024-08-13 14:45:57 -0500

    Updated OptionsMinerTest.

--- library/shared/src/test/scala/org/sireum/OptionsMinerTest.scala
+++ library/shared/src/test/scala/org/sireum/OptionsMinerTest.scala
@@ -35,29 +35,29 @@ class OptionsMinerTest extends TestSuite {
 
     * - check(HashMap.empty, "// #Sireum")
 
-    * - check(HashMap.empty + "Logika" ~> "--sat",
+    * - check(HashMap.empty + "Logika" ~> ISZ[String]("--sat"),
       s"""// #Sireum
-         |
+         |//
          |//@Logika: --sat
          |
          |import org.sireum._"""
     )
 
-    * - check(HashMap.empty + "Logika" ~> "--sat",
+    * - check(HashMap.empty + "Logika" ~> ISZ[String]("--sat"),
       s"""// #Sireum
          |//@Logika: --sat
          |
          |import org.sireum._"""
     )
 
-    * - check(HashMap.empty + "Logika" ~> "--interprocedural",
+    * - check(HashMap.empty + "Logika" ~> ISZ[String]("--interprocedural", "--sat"),
       s"""// #Sireum
          |//@Logika: --interprocedural
          |//@Logika: --sat
          |import org.sireum._"""
     )
 
-    * - check(HashMap.empty + "Logika" ~> "--interprocedural --sat" + "InfoFlow" ~> "",
+    * - check(HashMap.empty + "Logika" ~> ISZ[String]("--interprocedural --sat") + "InfoFlow" ~> ISZ[String](""),
       s"""// #Sireum
          |//@Logika: --interprocedural \\
          |//@        --sat
@@ -66,7 +66,7 @@ class OptionsMinerTest extends TestSuite {
     )
   }
 
-  def check(expected: HashMap[String, String], text: Predef.String): Unit = {
+  def check(expected: HashMap[String, ISZ[String]], text: Predef.String): Unit = {
     assert(LibUtil.mineOptions(text.stripMargin) == expected)
   }
 
