d5802b88d17a552dd284819f7da0dcb39f876b51
Robby <robby@santoslab.org>
2025-03-18 08:08:47 -0500

    Directly build JSON AST when using ANTLRv3 JSON parser/lexer.

--- library/jvm/src/main/scala/org/sireum/parser/json/ParserUtil.scala
+++ library/jvm/src/main/scala/org/sireum/parser/json/ParserUtil.scala
@@ -2,21 +2,17 @@
 package org.sireum.parser.json
 
 import org.sireum._
-import org.sireum.parser.ParseTree
 
 object ParserUtil {
   @ext("ParserUtil_Ext") object Ext {
     def parseJson(uriOpt: Option[String],
                   input: String,
-                  reporter: message.Reporter): Option[ParseTree] = $
+                  reporter: message.Reporter): Option[AST] = $
   }
 
   def build(uriOpt: Option[String],
             input: String,
             reporter: message.Reporter): Option[AST] = {
-    Ext.parseJson(uriOpt, input, reporter) match {
-      case Some(t) => return Some(AST.build(t))
-      case _ => return None()
-    }
+    return Ext.parseJson(uriOpt, input, reporter)
   }
 }
--- library/jvm/src/main/scala/org/sireum/parser/json/ParserUtil_Ext.scala
+++ library/jvm/src/main/scala/org/sireum/parser/json/ParserUtil_Ext.scala
@@ -1,14 +1,148 @@
 package org.sireum.parser.json
 
-import org.antlr.runtime.{ANTLRStringStream, CommonTokenStream, RecognitionException}
+import org.antlr.runtime.tree.TreeAdaptor
+import org.antlr.runtime.{ANTLRStringStream, CommonTokenStream, RecognitionException, TokenStream}
 import org.sireum._
-import org.sireum.parser.{Antlr3Util, ParseTree}
+import org.sireum.helper.halt
+
+import scala.collection.mutable.ListBuffer
 
 object ParserUtil_Ext {
+  import org.antlr.runtime.{Token => AntlrToken}
+
+  private final case class Node(buffer: ListBuffer[Any]) {
+    def asLeaf: AntlrToken = buffer.head.asInstanceOf[AntlrToken]
+    def pos(docInfo: message.DocInfo): message.Position = {
+      var r: message.Position = ParserUtil_Ext.pos(docInfo, buffer.head.asInstanceOf[AntlrToken])
+      if (buffer.size > 1) r = r.to(ParserUtil_Ext.pos(docInfo, buffer.last.asInstanceOf[AntlrToken]))
+      r
+    }
+  }
+
+  private def pos(docInfo: message.DocInfo, token: AntlrToken): message.PosInfo = {
+    val offset = docInfo.lineOffsets(org.sireum.Z(token.getLine - 1)).toZ + org.sireum.Z(token.getCharPositionInLine)
+    val length = token.getText.length
+    val offsetLength = (org.sireum.conversions.Z.toU64(offset) << org.sireum.U64(32)) | org.sireum.U64(length)
+    org.sireum.message.PosInfo(docInfo, offsetLength)
+  }
+
+  private final class Adaptor(docInfo: message.DocInfo) extends TreeAdaptor {
+
+    def pos(token: AntlrToken): message.PosInfo = ParserUtil_Ext.pos(docInfo, token)
+
+    override def create(payload: AntlrToken): AntlrToken = {
+      payload
+    }
+
+    override def dupNode(treeNode: Any): AnyRef = halt("Infeasible")
+
+    override def dupTree(tree: Any): AnyRef = halt("Infeasible")
+
+    override def nil(): AnyRef = Node(ListBuffer())
+
+    override def errorNode(input: TokenStream, start: AntlrToken, stop: AntlrToken, e: RecognitionException): AnyRef = {
+      val name = Thread.currentThread.getStackTrace()(2).getMethodName
+      AST.Str(s"$name: ${if (e.getMessage == null) e.getClass.getName else e.getMessage}", pos(start).to(pos(stop)))
+    }
+
+    override def isNil(tree: Any): Boolean = halt("Infeasible")
+
+    override def addChild(t: Any, child: Any): Unit = {
+      val node = t.asInstanceOf[Node]
+      node.buffer.addOne(child)
+    }
+
+    override def becomeRoot(newRoot: Any, oldRoot: Any): Any = halt("Infeasible")
+
+    override def rulePostProcessing(root: Any): Any = {
+      val node = root.asInstanceOf[Node]
+      val name = Thread.currentThread.getStackTrace()(2).getMethodName
+      name match {
+        case "valueFile" => node.buffer.head
+        case "value" => node.buffer.head
+        case "object" =>
+          val kvs = ISZ((for (e <- node.buffer.toSeq if e.isInstanceOf[AST.KeyValue]) yield e.asInstanceOf[AST.KeyValue]): _*)
+          AST.Obj(kvs, node.pos(docInfo))
+        case "array" =>
+          val values = ISZ((for (e <- node.buffer.toSeq if e.isInstanceOf[AST]) yield e.asInstanceOf[AST]): _*)
+          AST.Arr(values, node.pos(docInfo))
+        case "keyValue" =>
+          val keyToken = node.asLeaf
+          val keyText = Json.Parser(conversions.String.toCis(keyToken.getText), 0, None()).parseString()
+          val key = AST.Str(keyText, pos(keyToken))
+          val value = node.buffer(2).asInstanceOf[AST]
+          AST.KeyValue(key, value)
+        case "stringLit" =>
+          val token = node.asLeaf
+          val text = Json.Parser(conversions.String.toCis(node.asLeaf.getText), 0, None()).parseString()
+          AST.Str(text, pos(token))
+        case "doubleLit" =>
+          val token = node.asLeaf
+          AST.Dbl(F64(token.getText).get, pos(node.asLeaf))
+        case "intLit" =>
+          val token = node.asLeaf
+          AST.Int(Z(token.getText).get, pos(node.asLeaf))
+        case "boolLit" =>
+          val token = node.asLeaf
+          AST.Bool("true" == token.getText, pos(node.asLeaf))
+        case "nullLit" => AST.Null(pos(node.asLeaf))
+        case _ => halt(s"Infeasible: $name")
+      }
+    }
+
+    override def getUniqueID(node: Any): Int = halt("Infeasible")
+
+    override def becomeRoot(newRoot: AntlrToken, oldRoot: Any): AnyRef = halt("Infeasible")
+
+    override def create(tokenType: Int, fromToken: AntlrToken): AnyRef = halt("Infeasible")
+
+    override def create(tokenType: Int, fromToken: AntlrToken, text: Predef.String): AnyRef = halt("Infeasible")
+
+    override def create(tokenType: Int, text: Predef.String): AnyRef = halt("Infeasible")
+
+    override def getType(t: Any): Int = halt("Infeasible")
+
+    override def setType(t: Any, `type`: Int): Unit = halt("Infeasible")
+
+    override def getText(t: Any): Predef.String = halt("Infeasible")
+
+    override def setText(t: Any, text: Predef.String): Unit = halt("Infeasible")
+
+    override def getToken(t: Any): AntlrToken = halt("Infeasible")
+
+    override def setTokenBoundaries(t: Any, startToken: AntlrToken, stopToken: AntlrToken): Unit = {
+      t match {
+        case _: Node =>
+        case _ => halt(s"Infeasible: $t")
+      }
+    }
+
+    override def getTokenStartIndex(t: Any): Int = halt("Infeasible")
+
+    override def getTokenStopIndex(t: Any): Int = halt("Infeasible")
+
+    override def getChild(t: Any, i: Int): AnyRef = halt("Infeasible")
+
+    override def setChild(t: Any, i: Int, child: Any): Unit = halt("Infeasible")
+
+    override def deleteChild(t: Any, i: Int): AnyRef = halt("Infeasible")
+
+    override def getChildCount(t: Any): Int = halt("Infeasible")
+
+    override def getParent(t: Any): AnyRef = halt("Infeasible")
+
+    override def setParent(t: Any, parent: Any): Unit = halt("Infeasible")
+
+    override def getChildIndex(t: Any): Int = halt("Infeasible")
+
+    override def setChildIndex(t: Any, index: Int): Unit = halt("Infeasible")
+
+    override def replaceChildren(parent: Any, startChildIndex: Int, stopChildIndex: Int, t: Any): Unit = halt("Infeasible")
+  }
 
   def parseJson(uriOpt: Option[String],
                 input: String,
-                reporter: message.Reporter): Option[ParseTree] = {
+                reporter: message.Reporter): Option[AST] = {
     val docInfo = message.DocInfo.create(uriOpt, input)
 
     val lex = new JSONLexer(new ANTLRStringStream(input.value)) {
@@ -30,8 +164,8 @@ object ParserUtil_Ext {
         reporter.error(Some(message.PosInfo(docInfo, offsetLength)), "JSONParser", msg)
       }
     }
-    r.setTreeAdaptor(new Antlr3Util.Adaptor(JSONParser.tokenNames, docInfo))
-    val tree = r.valueFile().tree.asInstanceOf[ParseTree]
+    r.setTreeAdaptor(new Adaptor(docInfo))
+    val tree = r.valueFile().tree.asInstanceOf[AST]
     return Some(tree)
   }
 
