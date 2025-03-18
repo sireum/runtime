2ce2cd28954bfb1aedeb99f76411b81efe20cfcf
Robby <robby@santoslab.org>
2025-03-18 16:01:12 -0500

    Enrinched JSON AST API.

--- library/shared/src/main/scala/org/sireum/parser/json/AST.scala
+++ library/shared/src/main/scala/org/sireum/parser/json/AST.scala
@@ -29,6 +29,8 @@ import org.sireum._
 
 @datatype trait AST {
   @pure def pos: message.Position
+  @pure def toST: ST
+  @pure def toCompactST: ST
 }
 
 
@@ -36,38 +38,77 @@ object AST {
 
   @datatype class Obj(val keyValues: ISZ[KeyValue], val pos: message.Position) extends AST {
     @strictpure def asMap: Map = Map(HashSMap ++ (for (kv <- keyValues) yield (kv.id.value, kv.value)))
+    @strictpure def toST: ST =
+      st"""{
+          |  ${(for (kv <- keyValues) yield kv.toST, ",\n")}
+          |}"""
+    @strictpure override def toCompactST: ST = st"{${(for (kv <- keyValues) yield kv.toCompactST, ",")}}"
   }
 
   @datatype class Map(val value: HashSMap[String, AST]) {
-    @strictpure def getOpt(id: String): Option[AST] = value.get(id)
     @strictpure def get(id: String): AST = getOpt(id).get
+    @strictpure def getOpt(id: String): Option[AST] = value.get(id)
     @strictpure def getObj(id: String): Obj = value.get(id).get.asInstanceOf[Obj]
+    @strictpure def getObjOpt(id: String): Option[Obj] = value.get(id).map((o: AST) => o.asInstanceOf[Obj])
     @strictpure def getArr(id: String): Arr = value.get(id).get.asInstanceOf[Arr]
+    @strictpure def getArrOpt(id: String): Option[Arr] = value.get(id).map((o: AST) => o.asInstanceOf[Arr])
     @strictpure def getInt(id: String): Int = value.get(id).get.asInstanceOf[Int]
+    @strictpure def getIntOpt(id: String): Option[Int] = value.get(id).map((o: AST) => o.asInstanceOf[Int])
+    @strictpure def getIntValueOpt(id: String): Option[Z] = value.get(id).map((o: AST) => o.asInstanceOf[Int].value)
     @strictpure def getDbl(id: String): Dbl = value.get(id).get.asInstanceOf[Dbl]
+    @strictpure def getDblOpt(id: String): Option[Dbl] = value.get(id).map((o: AST) => o.asInstanceOf[Dbl])
+    @strictpure def getDblValueOpt(id: String): Option[F64] = value.get(id).map((o: AST) => o.asInstanceOf[Dbl].value)
     @strictpure def getStr(id: String): Str = value.get(id).get.asInstanceOf[Str]
+    @strictpure def getStrOpt(id: String): Option[Str] = value.get(id).map((o: AST) => o.asInstanceOf[Str])
+    @strictpure def getStrValueOpt(id: String): Option[String] = value.get(id).map((o: AST) => o.asInstanceOf[Str].value)
     @strictpure def getBool(id: String): Bool = value.get(id).get.asInstanceOf[Bool]
+    @strictpure def getBoolOpt(id: String): Option[Bool] = value.get(id).map((o: AST) => o.asInstanceOf[Bool])
+    @strictpure def getBoolValueOpt(id: String): Option[B] = value.get(id).map((o: AST) => o.asInstanceOf[Bool].value)
     @strictpure def getNull(id: String): Null = value.get(id).get.asInstanceOf[Null]
-    @strictpure def getStrValueOpt(id: String): Option[String] = value.get(id) match {
-      case Some(v: AST.Str) => Some(v.value)
-      case _ => None()
-    }
+    @strictpure def getNullOpt(id: String): Option[Null] = value.get(id).map((o: AST) => o.asInstanceOf[Null])
 
   }
 
-  @datatype class KeyValue(val id: Str, val value: AST)
+  @datatype class KeyValue(val id: Str, val value: AST) {
+    @strictpure def toST: ST = st"${id.toST}: ${value.toST}"
+    @strictpure def toCompactST: ST = st"${id.toCompactST}:${value.toCompactST}"
+  }
 
-  @datatype class Arr(val values: ISZ[AST], val pos: message.Position) extends AST
+  @datatype class Arr(val values: ISZ[AST], val pos: message.Position) extends AST {
+    @strictpure def toST: ST =
+      if (values.isEmpty) st"[]"
+      else if (values(0).isInstanceOf[AST.Obj] || values(0).isInstanceOf[AST.Arr])
+        st"""[
+            |  ${(for (v <- values) yield v.toST, ",\n")}
+            |]"""
+      else st"[ ${(for (v <- values) yield v.toST, ", ")} ]"
+    @strictpure def toCompactST: ST = st"[${(for (v <- values) yield v.toCompactST,",")}]"
+  }
 
-  @datatype class Int(val value: Z, val pos: message.Position) extends AST
+  @datatype class Int(val value: Z, val pos: message.Position) extends AST {
+    @strictpure def toST: ST = toCompactST
+    @strictpure def toCompactST: ST = Json.Printer.printZ(value)
+  }
 
-  @datatype class Dbl(val value: F64, val pos: message.Position) extends AST
+  @datatype class Dbl(val value: F64, val pos: message.Position) extends AST {
+    @strictpure def toST: ST = toCompactST
+    @strictpure def toCompactST: ST = Json.Printer.printF64(value)
+  }
 
-  @datatype class Str(val value: org.sireum.String, val pos: message.Position) extends AST
+  @datatype class Str(val value: String, val pos: message.Position) extends AST {
+    @strictpure def toST: ST = toCompactST
+    @strictpure def toCompactST: ST = Json.Printer.printString(value)
+  }
 
-  @datatype class Bool(val value: B, val pos: message.Position) extends AST
+  @datatype class Bool(val value: B, val pos: message.Position) extends AST {
+    @strictpure def toST: ST = toCompactST
+    @strictpure def toCompactST: ST = Json.Printer.printB(value)
+  }
 
-  @datatype class Null(val pos: message.Position) extends AST
+  @datatype class Null(val pos: message.Position) extends AST {
+    @strictpure def toST: ST = toCompactST
+    @strictpure def toCompactST: ST = st"null"
+  }
 
   def build(tree: parser.ParseTree): AST = {
     @strictpure def asNode(t: parser.ParseTree): parser.ParseTree.Node = t.asInstanceOf[parser.ParseTree.Node]
